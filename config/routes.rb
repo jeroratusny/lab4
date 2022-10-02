8ab8f60a398c22827853e718c2066733ebf25dd7
jeroratusny <jeroratusny@gmail.com>
2022-10-02 15:02:00 +0000

    Preguntar redirect show.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   resources :monsters, only: [:index, :show]
-  resources :tweets, except: [:edit, :update]
+  resources :tweets, except: [:edit, :update]  
 
 
  # root "monsters#index"
   root "tweets#index"
+
+  # Resource Routing: the Rails Defauls Resource routing allows you to
+  # quickly declare all of the common routes for a given resourceful controller.
+  # A single call to resources can declare all of the necessary routes for your
+  # index, show, new, edit, create, update, and destroy actions.
 end
